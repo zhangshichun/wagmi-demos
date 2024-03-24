bfb5b45c9d4dd3404ab41a4e8f621938a64e44c4
张仕春 <zhangshichun_looper@aliyun.com>
2024-03-24 20:48:49 +0800

    chore: words wrong

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start">
-      <p>Hi, guys, here is the demos:</p>
+      <p>Hi guys, here are the demos:</p>
       <p>
         <Link href="/01-wallet-connect">01、wallet-connect</Link>
       </p>
